026eaeb66f2b45300d414cf5a203db103c0f06f3
realn7 <tolayshapa@gmail.com>
2025-04-13 13:25:42 +1000

    вынес в отдельный файлы обработку вывода, экран и переход

---
+++ input_handler.py
@@ -0,0 +1,26 @@
+import os
+import msvcrt
+
+# def get_char():
+#     return msvcrt.getch().decode('utf-8').lower()
+
+def get_char():
+    try:
+        if os.name == 'nt':  
+            ch = msvcrt.getch()
+            if ch == b'\xe0': 
+                ch = msvcrt.getch()
+                return None
+            return ch.decode('latin-1').lower()
+        else:  
+            import tty, termios
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch.lower()
+    except:
+        return None
